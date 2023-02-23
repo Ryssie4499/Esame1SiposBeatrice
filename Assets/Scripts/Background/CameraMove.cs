b9782bc3b6eb3163621ff92e8f9dbf5fbd2df2b6
Beatrice Sipos <sipibea2001@gmail.com>
2023-02-23 15:06:33 +0100

    Inizio Polishing

--- Assets/Scripts/Background/CameraMove.cs
+++ Assets/Scripts/Background/CameraMove.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    [HideInInspector] public bool cameraStop;
+    [HideInInspector] public bool cameraStop;                   //a contatto col trigger della fine del livello, la booleana diventa true
     [SerializeField] float cameraSpeed;                         //velocità della camera editabile
+
     GameManager GM;
 
     private void Start()
     {
         GM = FindObjectOfType<GameManager>();
     }
+
     void Update()
     {
         if (GM.gameStatus == GameManager.GameStatus.gameRunning)
         {
-
             //la camera si muove a velocità costante verso destra
             if (cameraStop == false)
             {
                 Camera.main.transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
             }
+            //la velocità della camera si azzera e la visuale si ferma
             else
             {
                 Camera.main.transform.Translate(Vector3.right * 0 * Time.deltaTime);
